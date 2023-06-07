1476d90699ec29677639160f98778ad393d5b740
Shreyansh Tiwari <shreyansh.tiwari@applyboard.com>
2023-06-08 00:59:36 +0530

    fix

--- src/Contexts/AddressContext.jsx
+++ src/Contexts/AddressContext.jsx
@@ -60,26 +60,26 @@ export default function AddressProvider({children}){
        
     }
 
-    useEffect(()=>{
-        ( (addressType) =>{
-            if(addressType === "add") {
-                setAddressList([...addressList,{...addState,_id:uuid() }]);
-                dispatch({type:'reset'});
-            } else {
-               const id = addState._id;
-                const newList = addressList.map(address => {
-                    if (address._id === id) {
-                      return { ...address, ...addState };
-                    } else {
-                      return address;
-                    }
-                  });
-                  setAddressList(newList);
-            }
+    // useEffect(()=>{
+    //     ( (addressType) =>{
+    //         if(addressType === "add") {
+    //             setAddressList([...addressList,{...addState,_id:uuid() }]);
+    //             dispatch({type:'reset'});
+    //         } else {
+    //            const id = addState._id;
+    //             const newList = addressList.map(address => {
+    //                 if (address._id === id) {
+    //                   return { ...address, ...addState };
+    //                 } else {
+    //                   return address;
+    //                 }
+    //               });
+    //               setAddressList(newList);
+    //         }
            
-        }
-    )('add');},
-    [addState,addressList]);
+    //     }
+    // )('add');},
+    // [addState,addressList]);
     
     return(
         <div>
