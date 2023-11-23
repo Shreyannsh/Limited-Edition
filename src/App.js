16b40049de70b5a86e4e93ce0939e6b268e80ef1
Shreyansh Tiwari <111145568+Shreyannsh@users.noreply.github.com>
2023-11-23 13:36:41 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-// import Mockman from 'mockman-js'
-import {NavLink,Routes,Route} from 'react-router-dom'
-import { FaShoppingCart,FaHeart,} from "react-icons/fa";
+//import Mockman from "mockman-js";
+import { NavLink, Routes, Route } from "react-router-dom";
+import { FaShoppingCart, FaHeart } from "react-icons/fa";
 import { ToastContainer } from "react-toastify";
 import Signup from "./Pages/Login/Signup/Signup";
 import Cart from "./Pages/Cart/Cart";
@@ -16,68 +16,104 @@ import Profile from "./Pages/Profile/Profile";
 import RequiresAuth from "./Context/RequiresAuth/RequiresAuth";
 import Checkout from "./Pages/Cart/Checkout/Checkout";
 import IndividualPage from "./Pages/IndividualPage/IndividualPage";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
-function App() { 
-
-  const {dispatch,state} = useContext(filterContext);
-  const {isLoggedIn} = useContext(authContext);
+function App() {
+  const { dispatch, state } = useContext(filterContext);
+  const { isLoggedIn } = useContext(authContext);
 
   return (
     <div className="App">
-       <ToastContainer
-                position="bottom-left"
-                autoClose={700}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-            />
+      <ToastContainer
+        position="bottom-left"
+        autoClose={700}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
 
-      <nav className='navBar'>
-        <NavLink className='nav-logo' to='/'>LIMITED EDITION</NavLink>
-        <input onChange={(e) =>dispatch({type:'productSearch', payload:e.target.value})} className='searchBar' placeholder='Search here' value={state.searchedText} type='text'/>
-        <NavLink to='/productList'className='explore'>Explore</NavLink>
-        <NavLink className='cart' to='/cart' ><FaShoppingCart/> <span>Cart</span></NavLink>
-        <NavLink className='wishList' to='/wishlist'><FaHeart/> <span>Wishlist</span></NavLink>
-        <NavLink className='login' to={isLoggedIn ? '/profile' :'/login'}>{isLoggedIn?'Profile':'Login'}</NavLink>
-      </nav> 
-    
+      <nav className="navBar">
+        <NavLink className="nav-logo" to="/">
+          LIMITED EDITION
+        </NavLink>
+        <input
+          onChange={(e) =>
+            dispatch({ type: "productSearch", payload: e.target.value })
+          }
+          className="searchBar"
+          placeholder="Search here"
+          value={state.searchedText}
+          type="text"
+        />
+        <NavLink to="/productList" className="explore">
+          Explore
+        </NavLink>
+        <NavLink className="cart" to="/cart">
+          <FaShoppingCart /> <span>Cart</span>
+        </NavLink>
+        <NavLink className="wishList" to="/wishlist">
+          <FaHeart /> <span>Wishlist</span>
+        </NavLink>
+        <NavLink className="login" to={isLoggedIn ? "/profile" : "/login"}>
+          {isLoggedIn ? "Profile" : "Login"}
+        </NavLink>
+      </nav>
+
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/cart' element={<RequiresAuth><Cart/></RequiresAuth>} />
-        <Route path='/wishlist' element={<RequiresAuth><WishList/></RequiresAuth>} />
-        <Route path='/productList' element={<ProductList/>} /> 
-        <Route path='/login' element={<Login/>} />
-        <Route path='/signup' element={<Signup/>} />
-        <Route path='/profile' element={<Profile/>} />
-        <Route path='/checkout' element ={<Checkout />} />
-        <Route path='/individualPage/:productId' element={<IndividualPage/>} />
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/cart"
+          element={
+            <RequiresAuth>
+              <Cart />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/wishlist"
+          element={
+            <RequiresAuth>
+              <WishList />
+            </RequiresAuth>
+          }
+        />
+        <Route path="/productList" element={<ProductList />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/individualPage/:productId" element={<IndividualPage />} />
       </Routes>
 
-     <footer>
-      <div className='footer-connect'>
-      <header><b>Connect</b></header>
-      <p className="connect">Instagram</p>
-      <p className="connect">Facebook</p>
-      <p className="connect">Twitter</p>
-      </div>
-      <div className='footer-address'>
-      <header><b>Adrress</b></header>
-      <p>Limited Edition,L143, Okhla- phase III, New Delhi 110046</p>
-      </div>
-      <div className='footer-contact'>
-      <header><b>Contact</b></header>
-      <p>011-3333-4444-88</p>
-      <p>011-9999-555-111</p>
-      <p>limitedEditon@gmail.com</p>
-      </div>
-     
-     </footer>    
+      <footer>
+        <div className="footer-connect">
+          <header>
+            <b>Connect</b>
+          </header>
+          <p className="connect">Instagram</p>
+          <p className="connect">Facebook</p>
+          <p className="connect">Twitter</p>
+        </div>
+        <div className="footer-address">
+          <header>
+            <b>Adrress</b>
+          </header>
+          <p>Limited Edition,L143, Okhla- phase III, New Delhi 110046</p>
+        </div>
+        <div className="footer-contact">
+          <header>
+            <b>Contact</b>
+          </header>
+          <p>011-3333-4444-88</p>
+          <p>011-9999-555-111</p>
+          <p>limitedEditon@gmail.com</p>
+        </div>
+      </footer>
     </div>
   );
 }
--- src/Contexts/AuthContext.jsx
+++ src/Contexts/AuthContext.jsx
@@ -1,125 +1,138 @@
 import { createContext, useState, useReducer } from "react";
-import axios from 'axios';
+import axios from "axios";
 import { authReducer } from "../Reducer/authReducer";
-import { useNavigate,useLocation } from "react-router-dom";
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate, useLocation } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const authContext = createContext();
- 
-export default function AuthProvider({children}){
 
-    const initializer = {
-        loginEmail:'',
-        loginPassword:'',
-        firstName:'',
-        lastName:'',
-        signupEmail:'',
-        signupPassword:'',
-        signupRePassword:''
+export default function AuthProvider({ children }) {
+  const initializer = {
+    loginEmail: "",
+    loginPassword: "",
+    firstName: "",
+    lastName: "",
+    signupEmail: "",
+    signupPassword: "",
+    signupRePassword: "",
+  };
+
+  const [state, dispatch] = useReducer(authReducer, initializer);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const [logInError, setLogInError] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = async () => {
+    try {
+      const credintials = {
+        email: state.loginEmail,
+        password: state.loginPassword,
+      };
+      console.log(credintials);
+      const response = await axios.post("/api/auth/login", credintials);
+
+      const encodedToken = response.data.encodedToken;
+      toast("Logged In!");
+      localStorage.setItem("encodedToken", encodedToken);
+      setLogInError("");
+      setIsLoggedIn(true);
+      let from = location.state?.from?.pathname || "/";
+      navigate(from);
+    } catch (error) {
+      toast.error("Invaild Credentials");
+    }
+  };
+
+  // const guestLogin = async() =>{
+  //     try{
+  //         const credintials = {
+  //             email:"adarshbalika@gmail.com",
+  //             password: "adarshbalika"
+  //         };
+
+  //         const response = await axios.post('/api/auth/login',credintials);
+
+  //         const encodedToken = response.data.encodedToken;
+
+  //         localStorage.setItem('encodedToken',encodedToken);
+  //         setLogInError('');
+  //         setIsLoggedIn(true);
+  //         let from = location.state?.from?.pathname || '/';
+  //         navigate(from);
+  //     }catch(error){
+  //         ////// (error);
+  //         setLogInError(error.message);
+  //     }
+  // }
+
+  const guestLogin = () => {
+    dispatch({ type: "guestLogin" });
+    login();
+  };
+
+  const signUp = async () => {
+    const cred = {
+      email: state.signupEmail,
+      password: state.signupPassword,
+      firstName: state.firstName,
+      lastName: state.lastName,
+    };
+    try {
+      const response = await axios.post("/api/auth/signup", cred);
+      const encodedToken = response.data.encodedToken;
+      localStorage.setItem("encodedToken", encodedToken);
+
+      toast("Great! Time to log In");
+
+      navigate("/login");
+    } catch (error) {
+      toast.error("Error from Server");
+      console.log(error);
+    }
+  };
+
+  const signUpDetails = () => {
+    if (
+      !state.firstName ||
+      !state.lastName ||
+      !state.signupEmail ||
+      !state.signupPassword ||
+      !state.signupRePassword
+    ) {
+      return toast.error("filling all fields are mandatory");
     }
 
-    const [state,dispatch] = useReducer(authReducer,initializer);
-
-    const navigate = useNavigate();
-    const location = useLocation();
-
-    const [logInError,setLogInError] = useState('');
-    const [isLoggedIn,setIsLoggedIn] = useState(false);
-
-  
-    const login = async() =>{
-        try{
-            const credintials = {
-                email: state.loginEmail,
-                password: state.loginPassword
-               
-            }
-            const response = await axios.post('/api/auth/login',credintials);
-
-            const encodedToken = response.data.encodedToken;
-            toast('Logged In!')
-            localStorage.setItem('encodedToken',encodedToken);
-            setLogInError('');
-            setIsLoggedIn(true);
-            let from = location.state?.from?.pathname || '/';
-            navigate(from);
-        }catch(error){
-            toast.error('Invaild Credentials')
-        }
+    if (state.signupPassword !== state.signupRePassword) {
+      return toast.error("Passwords do not match");
     }
 
-    // const guestLogin = async() =>{
-    //     try{
-    //         const credintials = {
-    //             email:"adarshbalika@gmail.com",
-    //             password: "adarshbalika" 
-    //         };
-
-    //         const response = await axios.post('/api/auth/login',credintials);
-
-    //         const encodedToken = response.data.encodedToken;
-
-    //         localStorage.setItem('encodedToken',encodedToken);
-    //         setLogInError('');
-    //         setIsLoggedIn(true);
-    //         let from = location.state?.from?.pathname || '/';
-    //         navigate(from);
-    //     }catch(error){
-    //         ////// (error);
-    //         setLogInError(error.message);
-    //     }
-    // }
-
-    const guestLogin = () =>{
-        dispatch({type:'guestLogin'})
-        login();
+    if (state.signupRePassword.length < 8) {
+      return toast.error("Password must be minimum 8 characters long");
     }
 
-    const signUp = async() =>{
-      const cred =  {
-            email: state.signupEmail,
-            password: state.signupPassword,
-            firstName: state.firstName,
-            lastName: state.lastName
-        }
-        try{
-            const response = await axios.post('/api/auth/signup',cred);
-            const encodedToken = response.data.encodedToken;
-            localStorage.setItem('encodedToken',encodedToken);
-
-            toast('Great! Time to log In')
-           
-            navigate('/login')
-    
-        }catch(error){
-            toast.error('Error from Server')
-            }
-    }
-
-    const signUpDetails = () =>{
-        
-        if(!state.firstName || !state.lastName || !state.signupEmail || !state.signupPassword || !state.signupRePassword ){
-            return   toast.error('filling all fields are mandatory')
-        
-        }
-
-        if (state.signupPassword !== state.signupRePassword ){
-           return   toast.error('Passwords do not match')
-        }
-
-        if(state.signupRePassword.length<8){
-           return   toast.error('Password must be minimum 8 characters long')
-        }
-
-        signUp()
-    }
-    console.log(state);
-    return(
-        <div >
-          <authContext.Provider value={{login,signUpDetails,dispatch,logInError,guestLogin,isLoggedIn,state,setIsLoggedIn}}>
-          {children}
-          </authContext.Provider>
-        </div>
-    )
+    signUp();
+  };
+  console.log(state);
+  return (
+    <div>
+      <authContext.Provider
+        value={{
+          login,
+          signUpDetails,
+          dispatch,
+          logInError,
+          guestLogin,
+          isLoggedIn,
+          state,
+          setIsLoggedIn,
+        }}
+      >
+        {children}
+      </authContext.Provider>
+    </div>
+  );
 }
